fa268d6fe050ab8cfc5b27f36479b2cd55279647
sergiokad <siarhei.rudak@gmail.com>
2022-07-26 20:38:44 +0400

    first contract version tested

--- hh/contracts/SoccerEventManager.sol
+++ hh/contracts/SoccerEventManager.sol
@@ -6,13 +6,18 @@ contract SoccerEventManager {
         string title;
         uint256 voteSize;
         address[] addresses;
-        uint8[] buckets;
+        uint16[] buckets;
         uint256 pot;
+        address[] payoutAddresses;
     }
     mapping (uint16 => Event) events;
     uint16 lastEventId;
+    address adminAddress;
+    address commissionAddress;
 
-    constructor() {
+    constructor(address _commissionAddress) {
+        commissionAddress = _commissionAddress;
+        adminAddress = msg.sender;
         lastEventId = 0;
     }
 
@@ -31,8 +36,9 @@ contract SoccerEventManager {
             _title,
             _voteSize,
             new address[](0),
-            new uint8[](0),
-            0
+            new uint16[](0),
+            0,
+            new address[](0)
         );
         events[lastEventId] = newEvent;
         return lastEventId;
@@ -40,7 +46,7 @@ contract SoccerEventManager {
 
     function submitVote(
         uint16 _eventId,
-        uint8 _bucketIdx
+        uint16 _bucketIdx
     ) external payable {
         require(msg.value == events[_eventId].voteSize, "Wrong voteSize");
         events[_eventId].addresses.push(address(msg.sender));
@@ -54,9 +60,33 @@ contract SoccerEventManager {
         return events[_eventId];
     }
 
-//    function payoutByEventId(
-//        uint16 _eventId
-//    ) {
-//
-//    }
+    function withdraw(address payable _receiverAddress, uint _receiverAmount) private {
+        _receiverAddress.transfer(_receiverAmount);
+    }
+
+    function payout(
+        uint16 _eventId,
+        uint16 _bucketIdx
+    ) public {
+        require(msg.sender == adminAddress, "Only admin can do it");
+        require(events[_eventId].addresses.length == events[_eventId].buckets.length, "Length of addresses not equals to length of votes");
+        require(events[_eventId].payoutAddresses.length == 0, "payoutAddresses was already filled");
+
+        uint commissionAmount = events[_eventId].pot / 100 * 7;
+        uint payoutAmount = events[_eventId].pot - commissionAmount;
+
+        withdraw(payable(commissionAddress), commissionAmount);
+
+        for (uint16 bIdx = 0; bIdx < events[_eventId].buckets.length; bIdx++) {
+            if (_bucketIdx == events[_eventId].buckets[bIdx]) {
+                events[_eventId].payoutAddresses.push(events[_eventId].addresses[bIdx]);
+            }
+        }
+
+        uint payoutPerAddress = payoutAmount / events[_eventId].payoutAddresses.length;
+
+        for (uint16 pIdx = 0; pIdx < events[_eventId].payoutAddresses.length; pIdx++) {
+            withdraw(payable(events[_eventId].payoutAddresses[pIdx]), payoutPerAddress);
+        }
+    }
 }
--- hh/test/SoccerEventManager.js
+++ hh/test/SoccerEventManager.js
@@ -1,33 +1,42 @@
-const { expect, assert } = require("chai");
+const { expect } = require("chai");
 // const { BN, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 const { ethers } = require("hardhat");
 
 let soccerEventManagerContract;
-let contractDeployer;
+let adminAccount;
+let adminAddress;
+let commissionWalletAccount;
+let commissionWalletAddress;
 let users;
 
 describe('TheBus SoccerEventManager testing...', function () {
 
   it('Prepare users', async function() {
     const signers = await ethers.getSigners();
-    contractDeployer = signers.slice(0, 1);
-    users = signers.slice(1, 9);
-    expect(contractDeployer.length).to.equal(1);
-    expect(users.length).to.equal(8);
+    const admin = signers.slice(0, 1);
+    const commissionWallet = signers.slice(1, 2);
+    users = signers.slice(2, 9);
+    adminAccount = admin[0];
+    adminAddress = adminAccount.address;
+    commissionWalletAccount = commissionWallet[0];
+    commissionWalletAddress = commissionWalletAccount.address;
+    expect(admin.length).to.equal(1);
+    expect(commissionWallet.length).to.equal(1);
+    expect(users.length).to.equal(7);
   });
 
   it('Deploys SoccerEventManager contract', async function () {
     const SoccerEventManager = await ethers.getContractFactory('SoccerEventManager');
-    soccerEventManagerContract = await SoccerEventManager.deploy();
+    soccerEventManagerContract = await SoccerEventManager.deploy(commissionWalletAddress);
   });
 
-  it('Checks that getEventsCount() returns 0 after deployment', async function() {
-    const count = await soccerEventManagerContract.getEventsCount();
-    expect(count).to.equal(0);
-  });
+  // it('Checks that getEventsCount() returns 0 after deployment', async function() {
+  //   const count = await soccerEventManagerContract.getEventsCount();
+  //   expect(count).to.equal(0);
+  // });
 
   it ('Creates first event and checks that getEventsCount() equals to 1', async function() {
-    await soccerEventManagerContract.setupEvent('first event test title', ethers.utils.parseEther("3"));
+    await soccerEventManagerContract.setupEvent('first event test title', ethers.utils.parseEther("10"));
     const count = await soccerEventManagerContract.getEventsCount();
     expect(count).to.equal(1);
   })
@@ -35,31 +44,31 @@ describe('TheBus SoccerEventManager testing...', function () {
   it ('Call getEventData(1) and checks the title, voteSize and pot', async function() {
     const eventData = await soccerEventManagerContract.getEventData(1);
     expect(eventData.title).to.equal('first event test title');
-    expect(eventData.voteSize).to.equal(ethers.utils.parseEther("3"));
+    expect(eventData.voteSize).to.equal(ethers.utils.parseEther("10"));
     expect(eventData.pot).to.equal(0);
   })
 
   it('Placing a votes by users and checking that pot increased properly', async function() {
-    await soccerEventManagerContract.connect(users[1]).submitVote(1, 1, {
-      value: ethers.utils.parseEther("3")
+    await soccerEventManagerContract.connect(users[0]).submitVote(1, 1, {
+      value: ethers.utils.parseEther("10")
+    });
+    await soccerEventManagerContract.connect(users[1]).submitVote(1, 0, {
+      value: ethers.utils.parseEther("10")
     });
     await soccerEventManagerContract.connect(users[2]).submitVote(1, 0, {
-      value: ethers.utils.parseEther("3")
+      value: ethers.utils.parseEther("10")
     });
-    await soccerEventManagerContract.connect(users[3]).submitVote(1, 0, {
-      value: ethers.utils.parseEther("3")
+    await soccerEventManagerContract.connect(users[3]).submitVote(1, 4, {
+      value: ethers.utils.parseEther("10")
     });
-    await soccerEventManagerContract.connect(users[4]).submitVote(1, 4, {
-      value: ethers.utils.parseEther("3")
+    await soccerEventManagerContract.connect(users[4]).submitVote(1, 5, {
+      value: ethers.utils.parseEther("10")
     });
-    await soccerEventManagerContract.connect(users[5]).submitVote(1, 5, {
-      value: ethers.utils.parseEther("3")
-    });
-    await soccerEventManagerContract.connect(users[1]).submitVote(1, 4, {
-      value: ethers.utils.parseEther("3")
+    await soccerEventManagerContract.connect(users[0]).submitVote(1, 4, {
+      value: ethers.utils.parseEther("10")
     });
     const eventData = await soccerEventManagerContract.getEventData(1);
-    expect(eventData.pot).to.equal(ethers.utils.parseEther("18"));
+    expect(eventData.pot).to.equal(ethers.utils.parseEther("60"));
   });
 
   it('Checks that it is not possible to send wrong voteSize', async function() {
@@ -75,23 +84,42 @@ describe('TheBus SoccerEventManager testing...', function () {
     ).to.be.revertedWith("Wrong voteSize");
   });
 
-  it('Checks that contract balance was increased to 18', async function() {
+  it('Checks that contract balance was increased to 60', async function() {
     const contractBalance = await ethers.provider.getBalance(soccerEventManagerContract.address);
-    expect(ethers.utils.formatEther(contractBalance.toString())).to.equal('18.0');
+    expect(ethers.utils.formatEther(contractBalance.toString())).to.equal('60.0');
+  });
+
+  it('Makes payout by invoking payout() method', async function() {
+    await soccerEventManagerContract.connect(adminAccount).payout(1, 4);
   });
 
   it('Print users balances', async function() {
-    printBalances(users);
+    await printBalances();
   });
-
-  // describe('', () => {
-  //
-  // });
 })
 
-function printBalances(users) {
-  users.map(async (user, i) => {
-    const userBalance = await ethers.provider.getBalance(user.address);
-    console.log(`users[${i}] balance:`, ethers.utils.formatEther(userBalance.toString()));
-  });
+async function printBalances() {
+  console.log('=== Balances ===');
+  const adminBalance = await ethers.provider.getBalance(adminAddress);
+  console.log('Admin balance:', ethers.utils.formatEther(adminBalance.toString()));
+  const commissionWalletBalance = await ethers.provider.getBalance(commissionWalletAddress);
+  console.log('Commission wallet balance:', ethers.utils.formatEther(commissionWalletBalance.toString()));
+  const contractBalance = await ethers.provider.getBalance(soccerEventManagerContract.address);
+  console.log('Contract balance:', ethers.utils.formatEther(contractBalance.toString()));
+
+  let userBalance;
+  userBalance = await ethers.provider.getBalance(users[0].address);
+  console.log(`users[0] balance:`, ethers.utils.formatEther(userBalance.toString()));
+  userBalance = await ethers.provider.getBalance(users[1].address);
+  console.log(`users[1] balance:`, ethers.utils.formatEther(userBalance.toString()));
+  userBalance = await ethers.provider.getBalance(users[2].address);
+  console.log(`users[2] balance:`, ethers.utils.formatEther(userBalance.toString()));
+  userBalance = await ethers.provider.getBalance(users[3].address);
+  console.log(`users[3] balance:`, ethers.utils.formatEther(userBalance.toString()));
+  userBalance = await ethers.provider.getBalance(users[4].address);
+  console.log(`users[4] balance:`, ethers.utils.formatEther(userBalance.toString()));
+  userBalance = await ethers.provider.getBalance(users[5].address);
+  console.log(`users[5] balance:`, ethers.utils.formatEther(userBalance.toString()));
+  userBalance = await ethers.provider.getBalance(users[6].address);
+  console.log(`users[6] balance:`, ethers.utils.formatEther(userBalance.toString()));
 }
